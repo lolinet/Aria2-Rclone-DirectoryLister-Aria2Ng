bda9417033924759b342f651af2215f631cf0b68
Chikage <33163118+chiakge@users.noreply.github.com>
2018-02-20 09:18:40 +0800

    Update trackers-list-aria2.sh

--- sh/trackers-list-aria2.sh
+++ sh/trackers-list-aria2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/sbin/service aria2 stop
+supervisorctl stop aria2d
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
@@ -8,3 +8,4 @@ else
     sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/aria2.conf
     echo update......
 fi
+supervisorctl start aria2d
